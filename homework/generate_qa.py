41830ea85bc17a78f246163cc74f23f674f0b0d9
Krithika-DS <krithika.kathire@tigeranalytics.com>
2025-05-01 14:43:46 -0500

    Add xtra q

--- homework/generate_qa.py
+++ homework/generate_qa.py
@@ -287,6 +287,10 @@ def generate_qa_pairs(info_path: str, view_index: int, img_width: int = 150, img
             "question": f"Is {k['kart_name']} in front of or behind the ego car?",
             "answer": vert
         })
+        qa.append({
+            "question": f"Where is {k['kart_name']} relative to the ego car?",
+            "answer": f"{vert} and {horiz}"
+        })
 
     # Counting
     qa.append({
